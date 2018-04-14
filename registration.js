51156f984f23ea072d7fb9e874f1616b55c39320
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-04-15 01:21:50 +0530

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,12 @@
+function collectData(argument) {
+	var from = document.getElementById('from').value;
+	var to = document.getElementById('to').value;
+}
+
+function displayData(argument) {
+	// body...
+}
+
+function deleteData(argument) {
+	// body...
+}
